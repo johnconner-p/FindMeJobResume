6634a5225ddda9cc30516c40a5b1426b2f24b758
Piyush <44675748+johnconner-p@users.noreply.github.com>
2026-02-19 15:11:24 +0530

    Add files via upload

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,8 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace with your actual Firebase Project Configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD5Zb5BPvVJ81BWq65M2W0rNa5BsCHEDPc",
   authDomain: "resumetrackin.firebaseapp.com",
@@ -10,9 +13,9 @@ const firebaseConfig = {
   storageBucket: "resumetrackin.firebasestorage.app",
   messagingSenderId: "647829258405",
   appId: "1:647829258405:web:558537b72e47744af10d4c",
+  measurementId: "G-GZ9Q64BD62"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export default app;
